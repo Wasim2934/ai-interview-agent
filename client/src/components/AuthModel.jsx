33b9ed7969261f496e0a08e221ebfb29dcddec3f
Wasim2934 <mdwasimlesnar0019@gmail.com>
2026-09-26 01:11:04 +0530

    change import statement for ./pages/Auth

--- client/src/components/AuthModel.jsx
+++ client/src/components/AuthModel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { FaTimes } from "react-icons/fa";
-import Auth from '../pages/auth';
+import Auth from '../pages/Auth';
 
 function AuthModel({onClose}) {
     const {userData} = useSelector((state)=>state.user)
@@ -15,7 +15,7 @@ function AuthModel({onClose}) {
     },[userData , onClose])
 
   return (
-    <div className='fixed inset-0 z-[999] flex items-center justify-center bg-black/10 backdrop-blur-sm px-4'>
+    <div className='fixed inset-0 z-999 flex items-center justify-center bg-black/10 backdrop-blur-sm px-4'>
         <div className='relative w-full max-w-md'>
             <button onClick={onClose} className='absolute top-8 right-5 text-gray-800 hover:text-black text-xl'>
              <FaTimes size={18}/>
